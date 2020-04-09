d32e7ff006eb3d564d5a79ab8bd29fc315c27217
accgit <zdenek.papucik@gmail.com>
2020-04-09 08:40:41 +0200

    dev

---
+++ src/Drago/User/DI/GravatarExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Extension
+ * Package built on Nette Framework
+ */
+
+namespace Drago\User\DI;
+
+use Drago;
+use Nette;
+
+
+class GravatarExtension extends Nette\DI\CompilerExtension
+{
+}
---
+++ src/Drago/User/Gravatar.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Extension
+ * Package built on Nette Framework
+ */
+
+namespace Drago\User;
+
+use Nette;
+
+
+class Gravatar
+{
+	use Nette\SmartObject;
+
+}
