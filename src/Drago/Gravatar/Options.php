80340b3d3ef566f7c91c158c8052613e9dec8a48
accgit <zdenek.papucik@gmail.com>
2026-05-23 17:12:18 +0200

    Update

--- src/Drago/Gravatar/Options.php
+++ src/Drago/Gravatar/Options.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace Drago\Gravatar;
 
 
-/** Configuration options for the Gravatar service. */
+/**
+ * Configuration options for the Gravatar service.
+ */
 final class Options
 {
-	/** Gravatar image size in pixels. */
 	public int $size = 80;
 
-	/** Default image type when no Gravatar exists. */
 	public string $defaultImage = 'mm';
 
-	/** Maximum allowed Gravatar rating. */
 	public string $rating = 'g';
 }
